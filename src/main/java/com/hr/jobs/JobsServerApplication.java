d83fdc7bc09faf4c7b3786f4cb3b09f0d425b52b
junngo <myeongjun.ko@gmail.com>
2023-05-01 23:53:56 +0900

    first commit

---
+++ src/main/java/com/hr/jobs/JobsServerApplication.java
@@ -0,0 +1,13 @@
+package com.hr.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hr/jobs/JobsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
